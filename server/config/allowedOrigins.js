ac9255d86fa3bfb33291847efc581253332f9802
Kushagra910 <kushagram63@gmail.com>
2024-05-03 23:31:14 +0530

    CORS updates

--- server/config/allowedOrigins.js
+++ server/config/allowedOrigins.js
@@ -3,7 +3,7 @@ const allowedOrigins = [
     // 'http://127.0.0.1:5500',
     // 'http://localhost:3500',
     'http://localhost:5173',
-    // 'http://codebuddyapp.netlify.app',
+    'code-verse-drab.vercel.app',
     // 'https://codebuddyapp.netlify.app',
 ];
 
